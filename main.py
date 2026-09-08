9540b0206747e971738a63b0a4e39621153942aa
Jovar19 <joaquin.vargas19@inacapmail.cl>
2026-09-08 15:16:53 -0300

    feat: pequeño recordatorio de python

---
+++ main.py
@@ -0,0 +1,45 @@
+# Tipos de variables
+variable_string: str = "Hola mundo"
+variable_numero_entero: int = 2026
+variable_numero_decimal: float = 1.5
+variable_booleano_falso: bool = False
+variable_booleano_verdadero: bool = True
+
+# Colecciones
+variable_lista: list = ["Manzana", "Pera", "Naranja", "Mandarina"]
+variable_tupla: tuple = (5, 2.5, 1, 0.5, 4, 4.5 )
+variable_diccionarios: dict = {"Nombre": "Sebastian", "color": = "Azul"}
+
+# Funciones
+def suma(a: int, b: int) -> int:
+    return a + b
+
+# Bucles
+conficion: bool = True
+while condicion == True:
+    print("Hola")
+
+    if input("¿Quieres salir?: ") == "Si":
+        condicion = False
+
+while True:
+    print("Hola")
+
+    if input("¿Quieres salir?: ") == "Si":
+        break
+
+# Iteracion
+lista_de_personas: list = ["Gabriel", "Jose", "Alejandro", "Javiera"]
+for persona in lista_de_personas:
+    print(persona)
+
+palabra: str = "Diccionario"
+for letra in palabra:
+    print(letra)
+
+for 1 in range(5):
+    print(1)
+
+# Estructuras de decision
+palabra_secreta = "123pormi"
+adivinacion = input("Adivina la palabra secreta: ")
